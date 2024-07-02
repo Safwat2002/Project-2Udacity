db74cba228812df1aec5313b187cf9573da8e7bd
safwat2002 <safwat.suliman.2002@gmail.com>
2024-07-02 19:54:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Landing Page Project
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+## Main Points
+ - this project mainly focus on how to use javascript Document object model api.
+- as well how to make documentation using comments and markdown language and how to imporove the code quality.
+
+## Main Tasks
+- this project require to dynamically create navbar items.
+- each navbar item navigate to a section.
+- when click on an navbar item it should be active
+the current section in the view should be distinguished by class named active with special style.
+- when clicking on a link the page scolls to corresponds section.
---
+++ js/app.js
@@ -0,0 +1,176 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+
+
+// this is simulation for data brought from an api contain section info
+
+var sectionsData = [
+  {
+    id: 1,
+    title: "Section 1",
+    link:"section1",
+    firstP:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+    secondP:
+      "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+  },
+  {
+    id: 2,
+    title: "Section 2",
+    link:"section2",
+    firstP:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+    secondP:
+      "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+  },
+  {
+    id: 3,
+    title: "Section 3",
+    link:"section3",
+    firstP:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+    secondP:
+      "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+  },
+  {
+    id: 4,
+    title: "Section 4",
+    link:"section4",
+    firstP:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+    secondP:
+      "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.",
+  },
+];
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+
+/**
+ * this function define the position of element where we want to scroll to
+ */
+function findPosition(obj) {
+  var currenttop = 0;
+  if (obj.offsetParent) {
+    do {
+      currenttop += obj.offsetTop;
+    } while ((obj = obj.offsetParent));
+    return [currenttop];
+  }
+}
+
+
+/**
+ * this function used to distinguish the currently active element by adding active class to it
+ */
+function makeActive(){
+  let sections = document.getElementById("sections").children;
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    //Find a value that works best, but 150 seems to be a good start.
+    if (box.top <= 400 && box.bottom >= 400) {
+      section.classList.add("active");
+    } else {
+      section.classList.remove("active");
+    //Remove active state from other section and corresponding Nav link
+    }
+  }
+}
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+// here we create a nav item for each section depends on data brought by
+// sectionsData
+for (let i = 0; i < sectionsData.length; i++) {
+  const item = document.createElement("li");
+  const link = document.createElement("a");
+  const node = document.createTextNode(sectionsData[i].title);
+  link.href = "#"+sectionsData[i].link;
+  link.appendChild(node);
+  item.appendChild(link);
+
+  item.classList.add("menu__link");
+  item.setAttribute('id',sectionsData[i].link+"_link" );
+
+  const element = document.getElementById("navbar__list");
+  element.appendChild(item);
+
+  // Scroll to anchor ID using scrollTO event
+  // important note : no need for this part since we used links to go to sections
+  // item.addEventListener("click",function(){
+  //   let destiniation = document.getElementById(sectionsData[i].link);
+  //   window.scrollTo(0,findPosition(destiniation));
+  // });
+  
+}
+
+/* This Code Is For Change Background for active navbar link */
+window.onload = function () {
+  document.getElementById("section1_link").classList.add("active-link");
+};
+window.onhashchange = function () {
+  let hash = window.location.hash;
+  hash = hash.slice(1).concat("_link");
+
+  let links = document.querySelectorAll(".menu__link");
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.remove("active-link");
+  }
+  document.getElementById(hash).classList.add("active-link");
+};
+
+
+
+// Add class 'active' to section when near top of viewport
+
+document.addEventListener("scroll",function(){
+  makeActive()
+});
+
+
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
